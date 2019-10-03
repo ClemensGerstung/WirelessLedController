d446c073b887f43da9934338fd896104e1bcbf1e
Clemens Gerstung <clemens8496@gmail.com>
2019-10-03 23:59:46 +0200

    init tcp server

--- Firmware/WirelessLedController.ino
+++ Firmware/WirelessLedController.ino
@@ -1,8 +1,28 @@
 #include "Arduino.h"
 
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+
+constexpr char* ssid = "<rename>";
+constexpr char* password = "<set>";
+
+WiFiServer server(80);
+
 void setup()
 {
+	Serial.begin(115200);
 
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+		Serial.println("Connecting...");
+	}
+
+	Serial.print("Connected to WiFi. IP: ");
+	Serial.println(WiFi.localIP());
+
+	server.begin();
 }
 
 
