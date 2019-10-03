20632d433e63c2484d88af7140585aff3a0b37dc
Clemens Gerstung <clemens8496@gmail.com>
2019-10-03 23:44:57 +0200

    startet firmware for ESP8266

---
+++ Firmware/sloeber.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2019-10-03 23:44:34
+
+#include "Arduino.h"
+#include "Arduino.h"
+
+void setup() ;
+void loop() ;
+
+#include "WirelessLedController.ino"
+
+
+#endif
