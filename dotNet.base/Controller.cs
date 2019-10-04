16ae6d5d38153905db621d19313be45424603003
Clemens Gerstung <clemens8496@gmail.com>
2019-10-04 21:18:15 +0200

    finished CLI and added comments for controller

--- dotNet.base/Controller.cs
+++ dotNet.base/Controller.cs
@@ -10,7 +10,10 @@ namespace WirelessLedController.Lib
   {
     public static short ToInt16(byte[] value, int startIndex)
     {
-      Array.Reverse(value);
+      if (System.BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(value);
+      }
 
       return System.BitConverter.ToInt16(value, startIndex);
     }
@@ -18,26 +21,37 @@ namespace WirelessLedController.Lib
     public static Array GetBytes(short value)
     {
       var array = System.BitConverter.GetBytes(value);
-      Array.Reverse(array);
+      if (System.BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(array);
+      }
+
       return array;
     }
 
     public static Array GetBytes(uint value)
     {
       var array = System.BitConverter.GetBytes(value);
-      Array.Reverse(array);
+      if (System.BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(array);
+      }
+
       return array;
     }
 
     public static uint ToUInt32(byte[] value, int startIndex)
     {
-      Array.Reverse(value);
+      if (System.BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(value);
+      }
 
       return System.BitConverter.ToUInt32(value, startIndex);
     }
   }
 
-  public enum Code : byte
+  internal enum Code : byte
   {
     Count = 0x01,
     SetLed = 0x02,
@@ -48,11 +62,14 @@ namespace WirelessLedController.Lib
     GetBrightness = 0x07
   }
 
-  public class Controller
+  public class Controller : IDisposable
   {
     private readonly TcpClient _client;
     private short? _ledCount;
 
+    /// <summary>
+    /// Gets the count of LEDs programmed onto the ESP
+    /// </summary>
     public short LedCount
     {
       get
@@ -74,13 +91,32 @@ namespace WirelessLedController.Lib
       }
     }
 
+    /// <summary>
+    /// Gets if the controller is connected to the ESP
+    /// </summary>
     public bool Connected => _client.Connected;
 
+    public string Host { get; }
+
+    public int Port { get; }
+
+    /// <summary>
+    /// Initializes the controller by connecting to the ESP
+    /// </summary>
+    /// <param name="host">Name or EndPoint-Name of the ESP</param>
+    /// <param name="port">Port on the ESP</param>
     public Controller(string host, int port)
     {
+      this.Host = host;
+      this.Port = port;
       _client = new TcpClient(host, port);
     }
 
+    /// <summary>
+    /// Sets the color of an single LED
+    /// </summary>
+    /// <param name="index">0 based index</param>
+    /// <param name="color">32bit color in RGBW</param>
     public void SetLedColor(short index, uint color)
     {
       byte[] data = new byte[7];
@@ -93,6 +129,11 @@ namespace WirelessLedController.Lib
       stream.Write(data, 0, data.Length);
     }
 
+    /// <summary>
+    /// Gets the color of an LED
+    /// </summary>
+    /// <param name="index">0-based index of the LED</param>
+    /// <returns>The color in RGBW</returns>
     public uint GetLedColor(short index)
     {
       byte[] data = new byte[4];
@@ -110,8 +151,19 @@ namespace WirelessLedController.Lib
       return 0;
     }
 
+    /// <summary>
+    /// Sets the colors of all LEDs in the given range
+    /// </summary>
+    /// <param name="from">First LED index (0-based)</param>
+    /// <param name="to">Last LED index (0-based, including)</param>
+    /// <param name="colors">Colors for the LEDs</param>
     public void SetLedColor(short from, short to, uint[] colors)
     {
+      if (colors.Length != (to - from))
+      {
+        throw new ArgumentException("Amount of colors must be the same as the range (to - from)");
+      }
+
       byte[] data = new byte[1 + 2 + 2 + colors.Length * 4];
       data[0] = (byte) Code.SetLeds;
       Array.Copy(BitConverter.GetBytes(from), 0, data, 1, 2);
@@ -129,6 +181,12 @@ namespace WirelessLedController.Lib
       stream.Write(data, 0, data.Length);
     }
 
+    /// <summary>
+    /// Gets the colors of a range of LEDs
+    /// </summary>
+    /// <param name="from">First LED index (0-based)</param>
+    /// <param name="to">Last LED index (0-based, including)</param>
+    /// <returns>Array of all colors in RGBW</returns>
     public uint[] GetLedColor(short from, short to)
     {
       byte[] data = new byte[5];
@@ -153,6 +211,10 @@ namespace WirelessLedController.Lib
       return colors;
     }
 
+    /// <summary>
+    /// Sets the color of all LEDs at the same time
+    /// </summary>
+    /// <param name="color">Color to use</param>
     public void SetLedColor(uint color)
     {
       uint[] colors = new uint[LedCount];
@@ -164,6 +226,10 @@ namespace WirelessLedController.Lib
       SetLedColor(0, LedCount, colors);
     }
 
+    /// <summary>
+    /// Sets the brightness of the leds
+    /// </summary>
+    /// <param name="b">Brightness to use</param>
     public void SetBrightness(byte b)
     {
       byte[] data = {(byte) Code.SetBrightness, b};
@@ -171,6 +237,10 @@ namespace WirelessLedController.Lib
       stream.Write(data, 0, 2);
     }
 
+    /// <summary>
+    /// Gets the current brightness of the LEDs
+    /// </summary>
+    /// <returns>The brightness</returns>
     public byte GetBrightness()
     {
       Stream stream = _client.GetStream();
@@ -183,6 +253,15 @@ namespace WirelessLedController.Lib
       }
 
       return (byte) r;
+    }
+
+    /// <summary>
+    /// Disposes the object
+    /// </summary>
+    public void Dispose()
+    {
+      _client?.Close();
+      _client?.Dispose();
     }
   }
 }
